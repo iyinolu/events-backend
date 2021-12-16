0d5ab4b497caa52bc7151ddbc6ca7591043a747d
Paul Osinowo <47259302+iyinolu@users.noreply.github.com>
2021-12-16 03:31:23 +0100

    filter event queryset based on user initiating a request

--- api/views.py
+++ api/views.py
@@ -30,7 +30,9 @@ class EventsViewSet(viewsets.ModelViewSet):
         given user by filtering against `eventdate` parameter in 
         Url.
         """
-        queryset = Events.objects.all()
+        # Filter queryset based on a user initiating the request
+        queryset = Events.objects.all().filter(owner=self.request.user.id)
+        
         event_date = self.request.query_params.get("eventdate")
         if event_date is not None:
             queryset = queryset.filter(event_date=event_date)
